b235e6c1b5c996466ec1043bb47278c46169a289
odera-dickens <dickens.gratitudeindia@gmail.com>
2021-02-08 12:39:12 -0800

    Added user Login API

--- app/Http/Controllers/Auth/Api/LoginController.php
+++ app/Http/Controllers/Auth/Api/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Auth;
+
 class LoginController extends Controller
 {
     public function __construct()
@@ -13,6 +15,13 @@ class LoginController extends Controller
     }
     public function login(Request $request)
     {
-        
+        if(Auth::attempt(['email'=> $request['email'],'password' => $request['password']]))
+        {
+            $user = Auth::user();
+            $token = $user->createToken('authToken')->accessToken;
+            return response()->json(['success'=> true, 'message'=>'You have successfully logged in'], 200);
+        }else{
+            return response()->json(['success'=> false, 'message'=>'Failed to login'], 500);
+        }
     }
 }
