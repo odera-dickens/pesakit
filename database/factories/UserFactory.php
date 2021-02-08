a2c8016454902fb6103ffa5456ace4ab3042c68a
odera-dickens <dickens.gratitudeindia@gmail.com>
2021-02-08 14:38:33 -0800

    Added a user with the admin role through seeding

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
-
+use Illuminate\Support\Facades\Hash;
 class UserFactory extends Factory
 {
     /**
@@ -23,11 +23,13 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
+            'name' => 'John Doe',
+            'email' => 'admin@test.com',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
+            'role' => 'admin',
+            'phone' => '0123456789'
         ];
     }
 }
