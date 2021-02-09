dc65f3b4ef7aa837286038c5d9da353aacc41f5f
odera-dickens <dickens.gratitudeindia@gmail.com>
2021-02-09 07:18:37 -0800

    Modified user filter function to return normal users

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -13,11 +13,11 @@ class AdminController extends Controller
     }
     public function getUsers()
     {
-        $users = User::all();
-        $filteredUsers = $users->filter(function($user){
+        //only show users with the role as 'user'
+        $users = User::get()->filter(function($user){ 
             return $user->role == 'user';
-        });
-        return view('admin.users.index', compact('users', $filteredUsers));
+         });
+        return view('admin.users.index', compact('users', $users));
     }
     public function getUser(User $user)
     {
